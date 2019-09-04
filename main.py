10680568329c33f6f1842c072c1bc90e1f1e6ec8
MattMoony <monschein.matthias@gmail.com>
2019-09-04 22:52:31 +0200

    updates ...

--- main.py
+++ main.py
@@ -31,17 +31,20 @@ class Net(nn.Module):
         for i in range(23, 30):
             self.sl_5.add_module(str(i), vgg_feats[i])
 
+        for p in self.parameters():
+            p.requires_grad_(False)
+
     def forward(self, x):
-        x = self.sl_1(x)
-        h_state_1 = x
-        x = self.sl_2(x)
-        h_state_2 = x
-        x = self.sl_3(x)
-        h_state_3 = x
-        x = self.sl_4(x)
-        h_state_4 = x
-        x = self.sl_5(x)
-        h_state_5 = x
+        h = self.sl_1(x)
+        h_state_1 = h
+        h = self.sl_2(h)
+        h_state_2 = h
+        h = self.sl_3(h)
+        h_state_3 = h
+        h = self.sl_4(h)
+        h_state_4 = h
+        h = self.sl_5(h)
+        h_state_5 = h
 
         return [h_state_1, h_state_2, h_state_3, h_state_4, h_state_5]
 
@@ -52,50 +55,54 @@ def show_t_img(t):
 def gram_mat(f):
     (b, c, h, w) = f.shape
     f = f.view(b, c, h * w)
-    g = f.bmm(f.transpose(1, 2))
-    return g / (c * h * w)
+    g = f.bmm(f.transpose(1, 2)) / (c * h * w)
+    return g
 
 def train(model, cont_img, style_img, transform, args):
     img = torch.randn(1, *cont_img.size(), requires_grad=True, device='cuda')
     # img = torch.ones(1, *cont_img.size(), requires_grad=True, device='cuda')
 
-    optimizer = optim.Adam([img], args.lr)
+    optimizer = optim.Adam([img], lr=args.lr)
     mse_loss = nn.MSELoss()
 
-    # model.eval()
     cont_feat = model(cont_img.view(1, *cont_img.size()))
     for f in cont_feat:
-        f.detach_()
+        f.requires_grad_(False)
         
-    gram_mats = [gram_mat(f) for f in model(style_img.view(1, *style_img.size()))]
-    for f in gram_mats:
-        f.detach_()
+    style_feat = model(style_img.view(1, *style_img.size()))
+    gram_mats = [gram_mat(f) for f in style_feat]
+    for g in gram_mats:
+        g.requires_grad_(False)
 
-    # model.train()
-    for i in range(args.iters):
-        optimizer.zero_grad()
-        out = model(img)
+    try:
+        for i in range(args.iters):
+            optimizer.zero_grad()
+            out = model(img)
+            # out = model(transform(img.view(*img.shape[1:])).view(*img.shape))
 
-        content_loss = args.cont_w * mse_loss(out[1], cont_feat[1])
+            content_loss = args.cont_w * mse_loss(out[1], cont_feat[1])
 
-        style_loss = mse_loss(gram_mat(out[0]), gram_mats[0])
-        for j in range(1, len(out)):
-            style_loss += mse_loss(gram_mat(out[j]), gram_mats[j])
-        style_loss *= args.style_w
+            style_loss = mse_loss(gram_mat(out[0]), gram_mats[0])
+            for j in range(1, len(out)):
+                style_loss += mse_loss(gram_mat(out[j]), gram_mats[j])
+            style_loss *= args.style_w
 
-        reg_loss = args.reg_w * (
-            torch.sum(torch.abs(img[:, :, :, 1:] - img[:, :, :, :-1])) +
-            torch.sum(torch.abs(img[:, :, 1:, :] - img[:, :, :-1, :]))
-        )
+            reg_loss = args.reg_w * (
+                torch.sum(torch.abs(img[:, :, :, 1:] - img[:, :, :, :-1])) +
+                torch.sum(torch.abs(img[:, :, 1:, :] - img[:, :, :-1, :]))
+            )
 
-        total_loss = content_loss + style_loss + reg_loss
-        total_loss.backward()
-        optimizer.step()
+            total_loss = content_loss + style_loss + reg_loss
+            total_loss.backward()
+            optimizer.step()
 
-        print('[epoch#{:04d}]: Total-Loss -> {:} ... '.format(i, total_loss.item()))
-        print('              Content-Loss -> {:} ... '.format(content_loss.item()))
-        print('              Style-Loss -> {:} ... '.format(style_loss.item()))
-        print('              Reg-Loss -> {:} ... '.format(reg_loss.item()))
+            print('[epoch#{:04d}]: Total-Loss\t-> {:} ... '.format(i, total_loss.item()))
+            print('              Content-Loss\t-> {:} ... '.format(content_loss.item()))
+            print('              Style-Loss\t-> {:} ... '.format(style_loss.item()))
+            print('              Reg-Loss\t\t-> {:} ... '.format(reg_loss.item()))
+            print('='*64)
+    except KeyboardInterrupt:
+        pass
 
     return img
 
@@ -103,7 +110,7 @@ def main():
     model = Net()
     model.to('cuda')
 
-    style_img = io.imread('media/style/gustav-klimt-kiss-scaled.jpg')
+    style_img = io.imread('media/style/gustav-klimt-kiss.jpg')
     style_img = style_img.reshape(3, *style_img.shape[:-1])
     cont_img = io.imread('media/content/parrots-kiss-scaled.jpg')
     cont_img = cont_img.reshape(3, *cont_img.shape[:-1])
@@ -118,7 +125,7 @@ def main():
     cont_img = transform(cont_img / 255.0)
 
     arguments = namedtuple('args', ['lr', 'iters', 'cont_w', 'style_w', 'reg_w'])
-    args = arguments(1, 128, 1, 1, 0)
+    args = arguments(0.06, 2048, 1., 1., 0.)
 
     img = train(model, cont_img, style_img, transform, args)
     show_t_img(img)
